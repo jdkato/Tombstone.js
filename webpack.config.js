d7f5df0a36af5d5f14cd5ac694eaae5406e5f1db
Joseph Kato <joseph@jdkato.io>
2017-04-29 09:32:11 -0700

    :wrench: capitalize lib name

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
 var path = require('path')
 var env = require('yargs').argv.mode
 
-var libraryName = 'tombstone'
+var libraryName = 'Tombstone'
 var plugins = []
 var outputFile
 
